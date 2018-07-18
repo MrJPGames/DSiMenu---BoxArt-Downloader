1d4e39399b8a93a000070d336e1cd5f7221a3680
Jasper W. D. M. Peters <jasper@fizazy.com>
2018-07-18 22:21:14 +0200

    Updated method (Only need to select SD card), new UI, cleaner code

--- DSiMenu++ BoxArt Downloader/Form1.Designer.cs
+++ DSiMenu++ BoxArt Downloader/Form1.Designer.cs
@@ -25,62 +25,25 @@
 		private void InitializeComponent() {
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.progressBar1 = new System.Windows.Forms.ProgressBar();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button3 = new System.Windows.Forms.Button();
 			this.label1 = new System.Windows.Forms.Label();
-			this.label2 = new System.Windows.Forms.Label();
 			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// progressBar1
 			// 
-			this.progressBar1.Location = new System.Drawing.Point(13, 129);
+			this.progressBar1.Location = new System.Drawing.Point(13, 109);
 			this.progressBar1.Name = "progressBar1";
 			this.progressBar1.Size = new System.Drawing.Size(271, 26);
 			this.progressBar1.TabIndex = 0;
 			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(253, 26);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(30, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "...";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.ChangeGamesDirectory);
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(253, 71);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(30, 23);
-			this.button2.TabIndex = 2;
-			this.button2.Text = "...";
-			this.button2.UseVisualStyleBackColor = true;
-			this.button2.Click += new System.EventHandler(this.ChangeBoxArtDirectory);
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(12, 29);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(235, 20);
-			this.textBox1.TabIndex = 3;
-			this.textBox1.Click += new System.EventHandler(this.ChangeGamesDirectory);
-			// 
-			// textBox2
-			// 
-			this.textBox2.Location = new System.Drawing.Point(12, 74);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(235, 20);
-			this.textBox2.TabIndex = 4;
-			this.textBox2.Click += new System.EventHandler(this.ChangeBoxArtDirectory);
-			// 
 			// button3
 			// 
-			this.button3.Location = new System.Drawing.Point(12, 100);
+			this.button3.Enabled = false;
+			this.button3.Location = new System.Drawing.Point(12, 80);
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(271, 23);
 			this.button3.TabIndex = 5;
@@ -93,31 +56,48 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(9, 9);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(86, 13);
+			this.label1.Size = new System.Drawing.Size(83, 13);
 			this.label1.TabIndex = 6;
-			this.label1.Text = "Games directory:";
+			this.label1.Text = "SD Drive Letter:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(13, 26);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 8;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(140, 24);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(143, 23);
+			this.button4.TabIndex = 9;
+			this.button4.Text = "Update Drive List";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.UpdateAvailableDrives);
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(9, 58);
+			this.label2.Location = new System.Drawing.Point(13, 54);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(87, 13);
-			this.label2.TabIndex = 7;
-			this.label2.Text = "Box-Art directory:";
+			this.label2.Size = new System.Drawing.Size(72, 13);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "Select a drive";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(296, 165);
+			this.ClientSize = new System.Drawing.Size(301, 144);
 			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.comboBox1);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button3);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
 			this.Controls.Add(this.progressBar1);
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "Form1";
@@ -130,14 +110,12 @@
 		#endregion
 
 		private System.Windows.Forms.ProgressBar progressBar1;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.TextBox textBox1;
-		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button3;
 		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
--- DSiMenu++ BoxArt Downloader/Form1.cs
+++ DSiMenu++ BoxArt Downloader/Form1.cs
@@ -12,68 +12,85 @@ using System.Windows.Forms;
 
 namespace DSiMenu___BoxArt_Downloader {
 	public partial class Form1 : Form {
-		private string gamesDirectory = "";
-		private string boxArtDirectory = "";
+		private static string boxArtDirectory = "_nds\\dsimenuplusplus\\boxart\\";
+		List<String> drives = new List<String>();
+
+		bool validDriveSelected = false;
+
 		public Form1() {
 			InitializeComponent();
+
+			//Detect all drives (Only USB will be added (SD is also seen as USB or "removable"))
+			UpdateAvailableDrives();
 		}
 
-		private void ChangeGamesDirectory(object sender, EventArgs e) {
-			// Show the FolderBrowserDialog.
-			DialogResult result = folderBrowserDialog1.ShowDialog();
-			if (result == DialogResult.OK) {
-				gamesDirectory = folderBrowserDialog1.SelectedPath;
-				textBox1.Text = gamesDirectory;
+		//Needed input for function to allow it as button click event call
+		private void UpdateAvailableDrives(object s = null, EventArgs e = null) {
+			//Clear drives list
+			drives.RemoveRange(0, drives.Count);
+
+			//Detect drives
+			System.IO.DriveInfo[] drivesListing = System.IO.DriveInfo.GetDrives();
+			foreach (var drive in drivesListing) {
+				System.IO.DriveType driveType = drive.DriveType;
+				string driveName = drive.Name; // C:\, E:\, etc:\
+				switch (driveType) {
+					case System.IO.DriveType.Removable:
+						// Usually a USB Drive
+						drives.Add(drive.Name);
+						break;
+				}
 			}
-		}
 
-		private void ChangeBoxArtDirectory(object sender, EventArgs e) {
-			// Show the FolderBrowserDialog.
-			DialogResult result = folderBrowserDialog1.ShowDialog();
-			if (result == DialogResult.OK) {
-				boxArtDirectory = folderBrowserDialog1.SelectedPath;
-				textBox2.Text = boxArtDirectory;
+			comboBox1.Items.Clear();
+			comboBox1.Text = "";
+
+			//Add drives to combobox
+			foreach(string d in drives) {
+				comboBox1.Items.Add(d);
 			}
 		}
 
 		private void StartBoxArtDownload(object sender, EventArgs e) {
-			Console.WriteLine(gamesDirectory);
-			Console.WriteLine(boxArtDirectory);
-			if (gamesDirectory != "" && boxArtDirectory != "" && Directory.Exists(gamesDirectory) && Directory.Exists(boxArtDirectory)) {
-				Console.WriteLine("Initial check done!");
+			int total = 0;
+			int current = 0;
+			int totalSuccesful = 0;
+
+			if (validDriveSelected) {
 				var ext = new List<string> { ".nds" };
-				var myFiles = Directory.GetFiles(gamesDirectory, "*.*", SearchOption.AllDirectories)
+				var myFiles = Directory.GetFiles(comboBox1.Text, "*.*", SearchOption.AllDirectories)
 					 .Where(s => ext.Contains(Path.GetExtension(s)));
 
 				//Really ugly but couldn't find a way to find the Length/Count of myFiles
-				int total = 0;
 				foreach (string f in myFiles) {
-					total++;
+					if (GetSystemType(f) != 0x03 && GetGameCode(f) != "####") 
+						total++;
 				}
-				int current = 0;
 				foreach (string f in myFiles) {
-					current++;
-					Console.WriteLine(current + " " +  total + " " +  (int)((float)current / total));
-					progressBar1.Value = (int)((float)current * 100 / total);
-					progressBar1.Invalidate();
-					progressBar1.Update();
 					string gameCode = GetGameCode(f);
 					if (gameCode != "####") {
 						//Skip homebrew titles!
-						int systemType = GetSystemType(f);
 						Bitmap boxArt = null;
-						if (systemType != 0x03) {
+						if (GetSystemType(f) != 0x03) {
 							//Game is NTR or DSi-Enhanced (DS game carts)
+							current++;
+							Console.WriteLine(current);
+							progressBar1.Value = (int)(((float)current / total)*100);
+							progressBar1.Invalidate();
+							progressBar1.Update();
 							boxArt = DownloadArtNTR(gameCode);
 						} else {
 							//Game is DSi-Exclusive or DSiWare
 							//Skip until a site/database of DSiWare cover art data is found
 						}
-						if (boxArt != null)
+						if (boxArt != null) {
+							totalSuccesful++;
 							SaveBoxArt(boxArt, gameCode);
+						}
 					}
 				}
 			}
+			label2.Text = totalSuccesful + "/" + total + " found and downloaded!";
 			button3.Text = "Download BoxArt!";
 		}
 
@@ -102,28 +119,14 @@ namespace DSiMenu___BoxArt_Downloader {
 				System.Net.WebResponse response = request.GetResponse();
 				System.IO.Stream responseStream = response.GetResponseStream();
 				return new Bitmap(responseStream);
-			} catch(Exception e) {
+			} catch(Exception) {
 			}
 			return null;
 		}
 
 		private void SaveBoxArt(Bitmap b, String gameCode) {
-			/*
-			Byte[] imageData = new Byte[128 * 115 * 2];
-			UInt16 dataPoint;
-			for (int i=0; i<256; i+=2) {
-				for (int j=0; j<115; j++) {
-					Color c = b.GetPixel(i/2, j);
-					//A1R5G5B5
-					dataPoint = (UInt16)(1 << 15 | (int)((c.R / 255.0) * 32) << 10 | (int)((c.G / 255.0)*32) | (int)((c.B / 255.0)*32));
-					imageData[i + j * 256] = (Byte)(dataPoint >> 8);
-					imageData[i + j * 256 + 1] = (Byte)(dataPoint);
-				}
-			}
-			File.WriteAllBytes(boxArtDirectory + "\\" + gameCode + ".bmp", imageData);
-			*/
 			Bitmap finalImage = ConvertTo16bpp(b);
-			finalImage.Save(boxArtDirectory + "\\" + gameCode + ".bmp", ImageFormat.Bmp);
+			finalImage.Save(comboBox1.Text + boxArtDirectory + gameCode + ".bmp", ImageFormat.Bmp);
 		}
 
 		public static Bitmap ConvertTo16bpp(Image img) {
@@ -131,6 +134,28 @@ namespace DSiMenu___BoxArt_Downloader {
 			using (var gr = Graphics.FromImage(bmp))
 				gr.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height));
 			return bmp;
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+			int total = 0;
+			if (Directory.Exists(comboBox1.Text + "_nds\\dsimenuplusplus\\boxart")) {
+				var ext = new List<string> { ".nds" };
+				var myFiles = Directory.GetFiles(comboBox1.Text, "*.*", SearchOption.AllDirectories)
+					 .Where(s => ext.Contains(Path.GetExtension(s)));
+
+				//Really ugly but couldn't find a way to find the Length/Count of myFiles
+				foreach (string f in myFiles) {
+					if (GetSystemType(f) != 0x03 && GetGameCode(f) != "####")
+						total++;
+				}
+				validDriveSelected = true;
+				label2.Text = total + " NTR roms detected";
+				button3.Enabled = true;
+			} else {
+				validDriveSelected = false;
+				label2.Text = "SD Card with no or old DSiMenu++";
+				button3.Enabled = false;
+			}
 		}
 	}
 }
